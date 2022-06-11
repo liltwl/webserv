7873756dd8df2028b0eff41dd0aef68ee3b99a28
omar taouil <70657213+liltwl@users.noreply.github.com>
2022-06-11 23:36:21 +0100

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -153,30 +153,33 @@ void Requeststup(int fd, Request& ss, pollfd &fds)
     cout << "||||||||||||||||||||||||||||||||||||||||||" << endl;
 }
 
-
-
-
-
-
 vector<string> allow_methodstup(vector<string> str)
 {
     vector<string> val;
     for (int i = 1; i < str.size(); i++)
     {
         val.push_back(split(str[i], ';')[0]);
-        cout << split(str[i], ';')[0] << " :hada ra ghir method" << endl;
     }
 
     return val;
 }
 
-void location_pars(server *ss, vector<server> &tmp, std::ifstream &file, vector<string> str)
+
+
+
+
+
+
+
+
+string location_pars(server *ss, vector<server> &tmp, std::ifstream &file, vector<string> str)
 {
     loc tmp1;
     string stmp = str[1];
     string line;
-
-    for (int j = 0 ; str[0].compare("}"); j++)
+    int k = 0;
+    cout << "++++++++++++++loca++++++++++++++" << endl;
+    for (int j = 0 ; (k = line.find("}")) == -1 && !file.eof(); j++)
     {
         getline(file, line, '\n');
         ft_spaceskip(line);
@@ -185,16 +188,21 @@ void location_pars(server *ss, vector<server> &tmp, std::ifstream &file, vector<
             tmp1.root = split(str[1], ';')[0];
         else if (!str[0].compare("allow_methods")&& str.size())
             tmp1.methods = allow_methodstup(str);
+        cout << line << ": location :" << endl;
     }
     ss->location[stmp] = tmp1;
+    cout << line.substr(k + 1, line.size()) << ": dlm" << endl;
+    return (line.substr(k + 1, line.size()));
 }
 
-void server_pars(vector<server> &tmp,std::ifstream &file, server *ss)
+int server_pars(vector<server> &tmp,std::ifstream &file, server *ss)
 {
     vector<string> str;
     string line;
+    int k = 0;
 
-    for (int i = 0; !file.eof(); i++)
+    cout << "-----------serv-----------" << endl;
+    for (int i = 0; (k = line.find("}")) == -1 && !file.eof(); i++)
     {
         line.clear();
         str.clear();
@@ -214,14 +222,14 @@ void server_pars(vector<server> &tmp,std::ifstream &file, server *ss)
         else if (!str[0].compare("client_body_limit")&& str.size())
             ss->setbody_limit(stoi(split(str[1], ';')[0]));
         else if (!str[0].compare("location")&& str.size())
-            location_pars(ss, tmp, file, str);
-        else if (!str[0].compare("}"))
-            break ;
+            line = location_pars(ss, tmp, file, str);
         else if (!str.empty())
             ss->setothers(line);
     }
+    return k;
 }
 
+int guard(int n, string err) { if (n == -1) { cout << (err) << endl; exit(1); } return n; }
 
 void serversetup(vector<server> &tmp,std::ifstream &file)
 {
@@ -239,50 +247,15 @@ void serversetup(vector<server> &tmp,std::ifstream &file)
         ss = new server();
         cout << str[0] << ":ddd" <<endl;
         if (!str[0].compare("server"))
-            server_pars(tmp, file, ss);
+            if (server_pars(tmp, file, ss) == -1)
+                guard(-1, "error: bad config file.");
         if (ss->get_name().size())
             tmp.insert(tmp.begin(), *ss);
         delete ss;
-
-
-
-        // if (!str[0].compare("server"))
-        // {
-        //     if (ss)
-        //     {
-        //         tmp.insert(tmp.begin(), *ss);
-        //         delete ss;
-        //     }
-        //     ss = new server();
-        // }
-        // else if (!str[0].compare("server_name")&& str.size())
-        //     ss->setname(split(str[1], ';')[0]);
-        // else if (!str[0].compare("listen")&& str.size())
-        //     ss->setadd(split(str[1], ':')[0],stoi(split(str[1], ':')[1]));
-        // else if (!str[0].compare("root")&& str.size())
-        //     ss->setroot(split(str[1], ';')[0]);
-        // else if (!str[0].compare("allow_methods") && str.size())
-        //     ss->setmethods(allow_methodstup(str));
-        // else if (!str[0].compare("autoindex")&& str.size())
-        //     ss->setautoindex(split(str[1], ';')[0] == "on" ? 1 : 0);
-        // else if (!str[0].compare("client_body_limit")&& str.size())
-        //     ss->setbody_limit(stoi(split(str[1], ';')[0]));
-        // else if (!str[0].compare("lo cation")&& str.size())
-        //     location_pars(ss, tmp, file, str);
-        // else if (!str.empty())
-        //     ss->setothers(line);
-        // line.clear();
-        // str.clear();
     }
-    // if (ss)
-    // {
-    //     tmp.insert(tmp.begin(), *ss);
-    //     delete ss;
-    // }
     cout << "|||||||||||||||||||||||*********||||||||||||||||||||||||||||" << endl;
 }
 
-int guard(int n, string err) { if (n == -1) { cout << (err) << endl; exit(1); } return n; }
 
 void servers(vector<server> &ss,pollfd *fds)
 {
